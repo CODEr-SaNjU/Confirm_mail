3a4981f4d0f06fbf8b42817fa2999428862ba38d
CoDEr-SaNjU <sanju2help@gmail.com>
2020-01-21 08:24:21 +0530

    change on setting file

--- confirmation_mail/mail_confirm/mail_confirm/settings.py
+++ confirmation_mail/mail_confirm/mail_confirm/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sanju2help@gmail.com'
-EMAIL_HOST_PASSWORD = 'loveuhmaa'
+EMAIL_HOST_USER = 'gmail id here'
+EMAIL_HOST_PASSWORD = 'gmail password'
 EMAIL_PORT = 587
 
 
